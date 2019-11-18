e8ec6f69f422c14b859ae0d8647cf46f064d5c8b
Francisco lucas (zeta) <lucasnasm@gmail.com>
2019-11-18 19:46:04 -0300

    Commit push heroku

---
+++ public/precache-manifest.4ec5b1c9be707802165556a313ff6d44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c887ac0750057a3cac",
+    "url": "/css/about.e976d7ef.css"
+  },
+  {
+    "revision": "1fe95619f5ab4e175722",
+    "url": "/css/app.a53a474a.css"
+  },
+  {
+    "revision": "b672173dbd9c61284ae7",
+    "url": "/css/chunk-vendors.4b8affb7.css"
+  },
+  {
+    "revision": "087403aad0169af882da6b5c8cd9a0a2",
+    "url": "/img/logo.087403aa.png"
+  },
+  {
+    "revision": "ccf9d95143acb53f6788111ec974bf70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13c887ac0750057a3cac",
+    "url": "/js/about.5d5066f7.js"
+  },
+  {
+    "revision": "1fe95619f5ab4e175722",
+    "url": "/js/app.cab3854f.js"
+  },
+  {
+    "revision": "b672173dbd9c61284ae7",
+    "url": "/js/chunk-vendors.0d6b7d17.js"
+  },
+  {
+    "revision": "21801d24559de3680099a628cfc902aa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
