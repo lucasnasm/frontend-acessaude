12995a49511889c7707e31a7a7354be969c1ae6b
Francisco lucas (zeta) <lucasnasm@gmail.com>
2019-11-19 13:31:51 -0300

    api fallback mode history

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const history = require('connect-history-api-fallback')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000));
+
+// Express server.
+const staticFileMiddleware = express.static(__dirname,+ '/public')
+app.use(staticFileMiddleware)
+app.use(history({
+ disableDotRule: true,
+ verbose: true
+}))
+app.use(staticFileMiddleware)
+app.listen(app.get('port'), () => {
+ console.log('App listening:' + app.get('port'))
+})
